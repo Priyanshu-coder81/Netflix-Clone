48203b4d36e7f6af6df9f99f69958f7d4de8dbcd
Priyanshu Verma <vpriyanshu864@gmail.com>
2024-07-02 18:27:41 +0530

    Fix some dynamic changes

--- jv.js
+++ jv.js
@@ -1,5 +1,4 @@
 let quesBox = document.querySelectorAll(".quesBox");
-
 quesBox.forEach(btn => {
     btn.addEventListener("click", () => {
         btn.querySelector(".subText").classList.toggle("hide");
